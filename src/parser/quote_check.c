07c7b606bae3695cafbf4cb354684ea807a62325
Yamini Konka <ykonka@2-E-5.42heilbronn.de>
2026-07-09 18:23:47 +0200

    parser[quote_check.c, syntax_check.c] are added and tested

--- src/parser/quote_check.c
+++ src/parser/quote_check.c
@@ -6,7 +6,40 @@
 /*   By: ykonka <ykonka@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/08 13:14:36 by ykonka            #+#    #+#             */
-/*   Updated: 2026/07/08 13:14:37 by ykonka           ###   ########.fr       */
+/*   Updated: 2026/07/09 17:20:14 by ykonka           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "parser.h"
+
+int	has_unclosed_quotes(const char *line)
+{
+	int	quote;
+
+	quote = 0;
+	while (*line)
+	{
+		if (!quote && (*line == '"' || *line == '\''))
+			quote = *line;
+		else if (quote && *line == quote)
+			quote = 0;
+		line++;
+	}
+	return (quote != 0);
+}
+
+// int	has_unclosed_quotes(const char *line)
+// {
+// 	char	quote;
+
+// 	quote = 0;
+// 	while (*line)
+// 	{
+// 		if (!quote && (*line == '\'' || *line == '"'))
+// 			quote = *line;
+// 		else if (quote && *line == quote)
+// 			quote = 0;
+// 		line++;
+// 	}
+// 	return (quote != 0);
+// }
---
+++ src/parser/syntax_check.c
@@ -0,0 +1,92 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   syntax_check.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ykonka <ykonka@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/09 16:56:06 by ykonka            #+#    #+#             */
+/*   Updated: 2026/07/09 17:46:45 by ykonka           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "lexer.h"
+#include "parser.h"
+
+int	is_redir_token(t_token_type type)
+{
+	if (type == TOK_REDIR_IN || type == TOK_REDIR_OUT || type == TOK_APPEND
+		|| type == TOK_HEREDOC)
+		return (1);
+	return (0);
+}
+
+int	print_syntax_error(char *token)
+{
+	printf("minishell: syntax error near unexpected token `%s'\n", token);
+	return (1);
+}
+
+static int	redir_checks(t_token *token)
+{
+	t_token	*next;
+
+	next = token->next;
+	if (!next)
+		return (print_syntax_error("newline"));
+	if (next->type == TOK_PIPE)
+		return (print_syntax_error("|"));
+	if (is_redir_token(next->type))
+		return (print_syntax_error(next->value));
+	if (next->type != TOK_WORD)
+		return (print_syntax_error(next->value));
+	return (0);
+}
+
+static int	pipe_checks(t_token *token)
+{
+	t_token	*next;
+
+	next = token->next;
+	if (!next)
+		return (print_syntax_error("newline"));
+	if (next->type == TOK_PIPE)
+		return (print_syntax_error("|"));
+	return (0);
+}
+
+static int	validate_end_nodes(t_token *tokens)
+{
+	t_token	*last;
+
+	if (!tokens)
+		return (0);
+	if (tokens->type == TOK_PIPE)
+		return (print_syntax_error("|"));
+	last = tokens;
+	while (last->next)
+		last = last->next;
+	if (last->type == TOK_PIPE)
+		return (print_syntax_error("|"));
+	return (0);
+}
+
+int	syntax_check_tokens(t_token *tokens)
+{
+	t_token	*cur;
+
+	if (!tokens)
+		return (0);
+	if (validate_end_nodes(tokens))
+		return (1);
+	cur = tokens;
+	while (cur)
+	{
+		if (cur->type == TOK_PIPE && pipe_checks(cur))
+			return (1);
+		if (is_redir_token(cur->type) && redir_checks(cur))
+			return (1);
+		cur = cur->next;
+	}
+	return (0);
+}
