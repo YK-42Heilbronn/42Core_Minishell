fe6ff66f360557b2dfed11f55477f98a8c0ce177
Yamini Konka <ykonka@2-C-9.42heilbronn.de>
2026-07-13 05:55:07 +0200

    feat(expander, env): tests are done, bugs are fixed

--- include/expander.h
+++ include/expander.h
@@ -6,20 +6,21 @@
 /*   By: ykonka <ykonka@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/07 17:29:32 by ykonka            #+#    #+#             */
-/*   Updated: 2026/07/12 20:43:38 by ykonka           ###   ########.fr       */
+/*   Updated: 2026/07/13 05:29:31 by ykonka           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 // Resolve environment variables and quote sensitive text
 #ifndef EXPAND_H
 # define EXPAND_H
-# include "parser.h"
 # include "libft.h"
+# include "parser.h"
 
 /* src/env/env_init.c */
 int		env_init(t_shell *shell, char **envp);
 t_env	*env_new_node(const char *key, const char *value);
 void	env_add_back(t_env **env, t_env *new_node);
+void	free_env(t_env **sh_env);
 
 /* src/env/env_utils.c */
 char	*env_get_value(t_env *env, const char *key);
@@ -27,7 +28,7 @@ int		env_set_value(t_env **env, const char *key, const char *value);
 int		env_unset_value(t_env **env, const char *key);
 
 /* src/env/env_to_array.c */
-void free_envp(char **envp);
+void	free_envp(char **envp);
 char	**env_to_array(t_env *env);
 
 // src/expand/expand_utils.c
@@ -49,4 +50,3 @@ int		expand_one_token(t_token *token, t_shell *shell);
 int		expand_tokens(t_token *tokens, t_shell *shell);
 
 #endif
-
--- tests/main_expander_tests.c
+++ tests/main_expander_tests.c
@@ -6,7 +6,7 @@
 /*   By: ykonka <ykonka@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/12 18:40:30 by ykonka            #+#    #+#             */
-/*   Updated: 2026/07/12 21:49:23 by ykonka           ###   ########.fr       */
+/*   Updated: 2026/07/13 05:09:20 by ykonka           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,157 +15,388 @@
 #include <string.h>
 #include "expander.h"
 #include "parser.h"
+#include "lexer.h"
 
-/* prototypes missing from header for now */
-t_env	*env_new_node(const char *key, const char *value);
-void	env_add_back(t_env **env, t_env *new_node);
-t_env	*split_envp_value(char *env_value);
-void	free_env(t_env **sh_env);
-int		env_init(t_shell *shell, char **envp);
-int		env_set_value(t_env **env, const char *key, const char *value);
-int		env_unset_value(t_env **env, const char *key);
-char	**env_to_array(t_env *env);
-void	free_envp(char **envp);
+static int	g_total = 0;
+static int	g_pass = 0;
 
-static void	print_result(char *label, int ok)
+static void	test_result(const char *name, int ok)
 {
+	g_total++;
 	if (ok)
-		printf("[OK] %s\n", label);
+	{
+		g_pass++;
+		printf("[OK]   %s\n", name);
+	}
 	else
-		printf("[FAIL] %s\n", label);
+		printf("[FAIL] %s\n", name);
 }
 
-static int	str_equal(char *a, char *b)
+static int	same_str(const char *a, const char *b)
 {
 	if (!a && !b)
 		return (1);
 	if (!a || !b)
 		return (0);
+	printf("a::%s||b::%s\n", a, b);
 	return (strcmp(a, b) == 0);
 }
 
-static void	print_env_array(char **envp)
-{
-	int	i;
+/* ---------------- MOCK ENV ---------------- */
 
-	i = 0;
-	while (envp && envp[i])
+static t_env	*mock_env_new(const char *key, const char *value)
+{
+	t_env	*node;
+
+	node = malloc(sizeof(t_env));
+	if (!node)
+		return (NULL);
+	node->key = strdup(key);
+	node->value = strdup(value);
+	node->next = NULL;
+	return (node);
+}
+
+static void	mock_env_add_back(t_env **env, t_env *new_node)
+{
+	t_env	*cur;
+
+	if (!env || !new_node)
+		return ;
+	if (!*env)
 	{
-		printf("envp[%d] = %s\n", i, envp[i]);
-		i++;
+		*env = new_node;
+		return ;
+	}
+	cur = *env;
+	while (cur->next)
+		cur = cur->next;
+	cur->next = new_node;
+}
+
+// char	*env_get_value(t_env *env, const char *key)
+// {
+// 	while (env)
+// 	{
+// 		if (env->key && strcmp(env->key, key) == 0)
+// 			return (env->value);
+// 		env = env->next;
+// 	}
+// 	return (NULL);
+// }
+
+static void	mock_free_env(t_env **env)
+{
+	t_env	*next;
+
+	while (env && *env)
+	{
+		next = (*env)->next;
+		free((*env)->key);
+		free((*env)->value);
+		free(*env);
+		*env = next;
 	}
 }
 
-static void	test_env_init(t_shell *shell)
-{
-	char	*fake_envp[] = {
-		"USER=ykonka",
-		"HOME=/home/ykonka",
-		"PATH=/usr/bin:/bin",
-		NULL
-	};
+/* ---------------- MOCK TOKENS ---------------- */
 
-	printf("\n--- test_env_init ---\n");
+static t_token	*mock_token_new(t_token_type type, const char *value,
+					int sq, int dq)
+{
+	t_token	*tok;
+
+	tok = malloc(sizeof(t_token));
+	if (!tok)
+		return (NULL);
+	tok->type = type;
+	tok->value = strdup(value);
+	tok->quoted_single = sq;
+	tok->quoted_double = dq;
+	tok->next = NULL;
+	return (tok);
+}
+
+static void	mock_free_tokens(t_token *tok)
+{
+	t_token	*next;
+
+	while (tok)
+	{
+		next = tok->next;
+		free(tok->value);
+		free(tok);
+		tok = next;
+	}
+}
+
+/* ---------------- TESTS ---------------- */
+
+static void	test_is_valid_var_start(void)
+{
+	test_result("is_valid_var_start alpha", is_valid_var_start('A') == 1);
+	test_result("is_valid_var_start underscore", is_valid_var_start('_') == 1);
+	test_result("is_valid_var_start digit", is_valid_var_start('1') == 0);
+	test_result("is_valid_var_start dollar", is_valid_var_start('$') == 0);
+}
+
+static void	test_is_valid_var_char(void)
+{
+	test_result("is_valid_var_char alpha", is_valid_var_char('Z') == 1);
+	test_result("is_valid_var_char digit", is_valid_var_char('9') == 1);
+	test_result("is_valid_var_char underscore", is_valid_var_char('_') == 1);
+	test_result("is_valid_var_char dash", is_valid_var_char('-') == 0);
+}
+
+static void	test_var_name_len(void)
+{
+	test_result("var_name_len USER", var_name_len("USER") == 4);
+	test_result("var_name_len USER123", var_name_len("USER123") == 7);
+	test_result("var_name_len _ABC9+", var_name_len("_ABC9+") == 5);
+	test_result("var_name_len empty", var_name_len("") == 0);
+	test_result("var_name_len NULL", var_name_len(NULL) == 0);
 
 	/*
-	** current env_add_back needs first node already present
-	** so we seed USER manually, then init the rest
+	** This matches your current implementation:
+	** first char is checked with is_valid_var_char(), not valid_var_start().
 	*/
-	shell->env = split_envp_value("USER=ykonka");
-	env_init(shell, &fake_envp[1]);
-
-	print_result("env_get_value USER",
-		str_equal(env_get_value(shell->env, "USER"), "ykonka"));
-	print_result("env_get_value HOME",
-		str_equal(env_get_value(shell->env, "HOME"), "/home/ykonka"));
-	print_result("env_get_value PATH",
-		str_equal(env_get_value(shell->env, "PATH"), "/usr/bin:/bin"));
+	test_result("var_name_len current behavior 1ABC", var_name_len("1ABC") == 4);
 }
 
-static void	test_env_set_value(t_shell *shell)
+static void	test_extract_var_name(void)
 {
-	printf("\n--- test_env_set_value ---\n");
+	char	*name;
 
-	print_result("update USER",
-		env_set_value(&shell->env, "USER", "alice"));
-	print_result("USER updated",
-		str_equal(env_get_value(shell->env, "USER"), "alice"));
+	name = extract_var_name("HOME=abc", 4);
+	test_result("extract_var_name HOME", same_str(name, "HOME"));
+	free(name);
 
-	print_result("add TEST",
-		env_set_value(&shell->env, "TEST", "42"));
-	print_result("TEST added",
-		str_equal(env_get_value(shell->env, "TEST"), "42"));
+	name = extract_var_name("_VAR123", 7);
+	test_result("extract_var_name _VAR123", same_str(name, "_VAR123"));
+	free(name);
+
+	name = extract_var_name(NULL, 4);
+	test_result("extract_var_name NULL", name == NULL);
+
+	name = extract_var_name("ABC", 0);
+	test_result("extract_var_name len 0", name == NULL);
 }
 
-static void	test_env_unset_value(t_shell *shell)
+static void	test_str_append_char(void)
 {
-	printf("\n--- test_env_unset_value ---\n");
+	char	*s;
 
-	/*
-	** remove TEST only, not head node, because current unset
-	** has a bug on head removal
-	*/
-	print_result("unset TEST",
-		env_unset_value(&(shell->env), "TEST"));
-	print_result("TEST removed",
-		env_get_value(shell->env, "TEST") == NULL);
+	s = str_append_char(NULL, 'A');
+	test_result("str_append_char NULL", same_str(s, "A"));
+	free(s);
+
+	s = strdup("AB");
+	s = str_append_char(s, 'C');
+	test_result("str_append_char existing", same_str(s, "ABC"));
+	free(s);
 }
 
-static void	test_env_to_array(t_shell *shell)
+static void	test_str_append_str(void)
 {
-	char	**envp;
+	char	*s;
 
-	printf("\n--- test_env_to_array ---\n");
-	envp = env_to_array(shell->env);
-	print_result("env_to_array not NULL", envp != NULL);
-	if (envp)
-		print_env_array(envp);
-	free_envp(envp);
+	s = str_append_str(NULL, "hello");
+	test_result("str_append_str NULL", same_str(s, "hello"));
+	free(s);
+
+	s = strdup("hello ");
+	s = str_append_str(s, "world");
+	test_result("str_append_str existing", same_str(s, "hello world"));
+	free(s);
+
+	s = strdup("abc");
+	s = str_append_str(s, NULL);
+	test_result("str_append_str NULL suffix", same_str(s, "abc"));
+	free(s);
 }
 
-static void	test_expand_word(t_shell *shell)
+static void	test_expand_exit_status(void)
 {
+	t_shell	shell;
+	char	*status;
+
+	memset(&shell, 0, sizeof(shell));
+	shell.last_status = 42;
+	status = expand_exit_status(&shell);
+	test_result("expand_exit_status 42", same_str(status, "42"));
+	free(status);
+
+	status = expand_exit_status(NULL);
+	test_result("expand_exit_status NULL shell", status == NULL);
+}
+
+static void	test_expand_word_basic(void)
+{
+	t_shell	shell;
 	char	*res;
 
-	printf("\n--- test_expand_word ---\n");
+	memset(&shell, 0, sizeof(shell));
+	mock_env_add_back(&(shell.env), mock_env_new("USER", "ykonka"));
+	mock_env_add_back(&(shell.env), mock_env_new("HOME", "/home/ykonka"));
+	mock_env_add_back(&(shell.env), mock_env_new("EMPTY", ""));
+	shell.last_status = 127;
 
-	shell->last_status = 127;
-
-	res = expand_word("$USER", shell, 0, 0);
-	print_result("expand $USER", str_equal(res, "alice"));
+	res = expand_word("$USER", &shell, 0, 0);
+	test_result("expand_word $USER", same_str(res, "ykonka"));
 	free(res);
 
-	res = expand_word("hello $HOME", shell, 0, 0);
-	print_result("expand hello $HOME",
-		str_equal(res, "hello /home/ykonka"));
+	res = expand_word("hello $USER", &shell, 0, 0);
+	test_result("expand_word hello $USER", same_str(res, "hello ykonka"));
 	free(res);
 
-	res = expand_word("$?", shell, 0, 0);
-	print_result("expand $?", str_equal(res, "127"));
+	res = expand_word("$HOME", &shell, 0, 0);
+	test_result("expand_word $HOME", same_str(res, "/home/ykonka"));
 	free(res);
 
-	res = expand_word("$NOT_FOUND", shell, 0, 0);
-	print_result("expand missing var", str_equal(res, ""));
+	res = expand_word("$EMPTY", &shell, 0, 0);
+	test_result("expand_word empty var", same_str(res, ""));
 	free(res);
 
-	res = expand_word("$USER", shell, 1, 0);
-	print_result("single quote blocks expansion", str_equal(res, "$USER"));
+	res = expand_word("$NOPE", &shell, 0, 0);
+	test_result("expand_word missing var", same_str(res, ""));
 	free(res);
+
+	res = expand_word("$?", &shell, 0, 0);
+	test_result("expand_word $?", same_str(res, "127"));
+	free(res);
+
+	mock_free_env(&shell.env);
+}
+
+static void	test_expand_word_edge_cases(void)
+{
+	t_shell	shell;
+	char	*res;
+
+	memset(&shell, 0, sizeof(shell));
+	mock_env_add_back(&shell.env, mock_env_new("USER", "ykonka"));
+	shell.last_status = 5;
+
+	res = expand_word(NULL, &shell, 0, 0);
+	test_result("expand_word NULL", res == NULL);
+
+	res = expand_word("$USER", &shell, 1, 0);
+	test_result("expand_word single quotes block", same_str(res, "$USER"));
+	free(res);
+
+	res = expand_word("$USER", &shell, 0, 1);
+	test_result("expand_word double quotes allow", same_str(res, "ykonka"));
+	free(res);
+
+	res = expand_word("$", &shell, 0, 0);
+	test_result("expand_word single dollar", same_str(res, "$"));
+	free(res);
+
+	res = expand_word("abc$", &shell, 0, 0);
+	test_result("expand_word trailing dollar", same_str(res, "abc$"));
+	free(res);
+
+	res = expand_word("$1", &shell, 0, 0);
+	test_result("expand_word invalid after dollar digit", same_str(res, "$1"));
+	free(res);
+
+	res = expand_word("$$", &shell, 0, 0);
+	test_result("expand_word double dollar literal", same_str(res, "$$"));
+	free(res);
+
+	res = expand_word("$-abc", &shell, 0, 0);
+	test_result("expand_word invalid after dollar dash", same_str(res, "$-abc"));
+	free(res);
+
+	res = expand_word("$USER$?", &shell, 0, 0);
+	test_result("expand_word adjacent expansions", same_str(res, "ykonka5"));
+	free(res);
+
+	/*
+	** This matches your current implementation:
+	** USERy is read as one variable name.
+	*/
+	res = expand_word("x$USERy", &shell, 0, 0);
+	test_result("expand_word current behavior x$USERy", same_str(res, "x"));
+	free(res);
+
+	mock_free_env(&shell.env);
+}
+
+static void	test_expand_one_token(void)
+{
+	t_shell	shell;
+	t_token	*tok;
+
+	memset(&shell, 0, sizeof(shell));
+	mock_env_add_back(&shell.env, mock_env_new("USER", "ykonka"));
+	shell.last_status = 99;
+
+	test_result("expand_one_token NULL token", expand_one_token(NULL, &shell) == 1);
+
+	tok = mock_token_new(TOK_PIPE, "|", 0, 0);
+	test_result("expand_one_token non-word success", expand_one_token(tok, &shell) == 1);
+	test_result("expand_one_token non-word unchanged", same_str(tok->value, "|"));
+	mock_free_tokens(tok);
+
+	tok = mock_token_new(TOK_WORD, "$USER", 0, 0);
+	test_result("expand_one_token word success", expand_one_token(tok, &shell) == 1);
+	test_result("expand_one_token word expanded", same_str(tok->value, "ykonka"));
+	mock_free_tokens(tok);
+
+	tok = mock_token_new(TOK_WORD, "$USER", 1, 0);
+	test_result("expand_one_token single quoted", expand_one_token(tok, &shell) == 1);
+	test_result("expand_one_token single quoted unchanged",
+		same_str(tok->value, "$USER"));
+	mock_free_tokens(tok);
+
+	mock_free_env(&shell.env);
+}
+
+static void	test_expand_tokens(void)
+{
+	t_shell	shell;
+	t_token	*a;
+	t_token	*b;
+	t_token	*c;
+
+	memset(&shell, 0, sizeof(shell));
+	mock_env_add_back(&shell.env, mock_env_new("USER", "ykonka"));
+	mock_env_add_back(&shell.env, mock_env_new("HOME", "/tmp"));
+	shell.last_status = 21;
+
+	test_result("expand_tokens NULL list", expand_tokens(NULL, &shell) == 1);
+
+	a = mock_token_new(TOK_WORD, "$USER", 0, 0);
+	b = mock_token_new(TOK_WORD, "$?", 0, 0);
+	c = mock_token_new(TOK_PIPE, "|", 0, 0);
+	a->next = b;
+	b->next = c;
+
+	test_result("expand_tokens list success", expand_tokens(a, &shell) == 1);
+	test_result("expand_tokens token1", same_str(a->value, "ykonka"));
+	test_result("expand_tokens token2", same_str(b->value, "21"));
+	test_result("expand_tokens token3 unchanged", same_str(c->value, "|"));
+
+	mock_free_tokens(a);
+	mock_free_env(&shell.env);
 }
 
 int	main(void)
 {
-	t_shell	shell;
-
-	memset(&shell, 0, sizeof(t_shell));
-
-	test_env_init(&shell);
-	test_env_set_value(&shell);
-	test_env_unset_value(&shell);
-	test_env_to_array(&shell);
-	test_expand_word(&shell);
-
-	free_env(&shell.env);
+	printf("=== expand module unit tests ===\n\n");
+	test_is_valid_var_start();
+	test_is_valid_var_char();
+	test_var_name_len();
+	test_extract_var_name();
+	test_str_append_char();
+	test_str_append_str();
+	test_expand_exit_status();
+	test_expand_word_basic();
+	test_expand_word_edge_cases();
+	test_expand_one_token();
+	test_expand_tokens();
+	printf("\nPassed %d/%d tests\n", g_pass, g_total);
 	return (0);
 }
 
