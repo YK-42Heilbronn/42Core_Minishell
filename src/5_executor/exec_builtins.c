e91d48677b98c49372eeca574447c7a1b2fc5db0
Inga Leongard <ileongar@2-B-7.42heilbronn.de>
2026-07-14 20:11:38 +0200

    improved is_buildin

--- src/5_executor/exec_builtins.c
+++ src/5_executor/exec_builtins.c
@@ -6,33 +6,30 @@
 /*   By: ileongar <ileongar@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/12 17:56:16 by ileongar          #+#    #+#             */
-/*   Updated: 2026/07/13 23:04:16 by ileongar         ###   ########.fr       */
+/*   Updated: 2026/07/14 20:09:05 by ileongar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "executor.h"
 
-int is_builtin(const char *cmd)
+int	is_builtin(const char *cmd)
 {
-    return (cmd && (!ft_strncmp(cmd, "echo", 5)
-            || !ft_strncmp(cmd, "pwd", 4)
-            || !ft_strncmp(cmd, "env", 4)
-            || !ft_strncmp(cmd, "cd", 3)
-            || !ft_strncmp(cmd, "export", 7)
-            || !ft_strncmp(cmd, "unset", 6)
-            || !ft_strncmp(cmd, "exit", 5)));
+	if (!cmd)
+		return (0);
+	return ((ft_strlen(cmd) == 2 && ft_strncmp(cmd, "cd", 2) == 0)
+		|| (ft_strlen(cmd) == 4 && ft_strncmp(cmd, "echo", 4) == 0)
+		|| (ft_strlen(cmd) == 3 && ft_strncmp(cmd, "env", 3) == 0)
+		|| (ft_strlen(cmd) == 4 && ft_strncmp(cmd, "exit", 4) == 0)
+		|| (ft_strlen(cmd) == 6 && ft_strncmp(cmd, "export", 6) == 0)
+		|| (ft_strlen(cmd) == 3 && ft_strncmp(cmd, "pwd", 3) == 0)
+		|| (ft_strlen(cmd) == 5 && ft_strncmp(cmd, "unset", 5) == 0));
 }
 
-// int	is_builtin(const char *cmd)
-// {
-// 	return (cmd && (!ft_strncmp(cmd, "echo", ft_strlen("echo"))
-// 			|| !ft_strncmp(cmd, "pwd", ft_strlen("pwd")) || !ft_strncmp(cmd,
-// 				"env", ft_strlen("env")) || !ft_strncmp(cmd, "cd",
-// 				ft_strlen("cd")) || !ft_strncmp(cmd, "export",
-// 				ft_strlen("export")) || !ft_strncmp(cmd, "unset",
-// 				ft_strlen("unset")) || !ft_strncmp(cmd, "exit",
-// 				ft_strlen("exit"))));
-// }
+int	exec_builtin(t_shell *shell, t_cmd cmd)
+{
+	
+}
+
 
 int	exec_builtin(t_shell *shell, t_cmd *cmd)
 {
